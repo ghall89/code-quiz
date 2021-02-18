a18f4d7f6a64f8edd97551844c1a13a714621bb4
Graham Hall <ghall89@me.com>
2021-02-18 13:22:02 -0500

    Added initial quiz functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+var quizArr = quizData();
+console.log(quizArr);
+
 var pageContent = document.querySelector("#content");
 var timerDisplay = document.querySelector("#timer");
 
@@ -25,6 +28,12 @@ var startScreen = function() {
     pageContent.appendChild(titleEl);
     pageContent.appendChild(introEl);
     pageContent.appendChild(startBtnEl);
+
+    document.querySelector("#start-btn").addEventListener("click", function(){
+        timer();
+        clearContent();
+        quizQuestion();
+    });
 }
 
 // Clear content to prepare for new content
@@ -32,29 +41,109 @@ var clearContent = function() {
     pageContent.innerHTML = "";
 }
 
-
 // Timer function
 var timer = function(){
-    timeLeft = 10;
+    timeLeft = 40;
 
+    timerDisplay.textContent = timeLeft;
     var countdown = setInterval(function(){
-        if (timeLeft >= 0) {
-            timerDisplay.textContent = timeLeft;
+        if (timeLeft > 0) {
             timeLeft--;
+            timerDisplay.textContent = timeLeft;
         } else {
             alert("Time's up!");
+            clearContent();
+            startScreen();
             clearInterval(countdown);
         }
     }, 1000);
 
+
 }
 
+// Draw questions
+var quizQuestion = function(){
+    q = 0;
+
+    var correctAnswer = quizArr[q].correctAnswer;
+
+    pageContent.className = "question-screen";
+
+    var quizQuestion = document.createElement("p")
+    quizQuestion.textContent = quizArr[q].question;
+    pageContent.appendChild(quizQuestion);
+
+    // Generate answers
+    for(i = 0; i < 4; i++) {
+        var quizAnswer = document.createElement("button");
+        if (i === 0) {
+            quizAnswer.id = "answer-1"
+            quizAnswer.textContent = quizArr[q].answer1;
+        } else if (i === 1) {
+            quizAnswer.id = "answer-2"
+            quizAnswer.textContent = quizArr[q].answer2;
+        } else if (i === 2) {
+            quizAnswer.id = "answer-3"
+            quizAnswer.textContent = quizArr[q].answer3;
+        } else if (i === 3) {
+            quizAnswer.id = "answer-4"
+            quizAnswer.textContent = quizArr[q].answer4;
+        }
+        pageContent.appendChild(quizAnswer);
+    }
+
+    document.querySelector("#answer-1").addEventListener("click", function(){
+        var inputAnswer = 1;
+        validateAnswer(inputAnswer, correctAnswer);
+    })
+    document.querySelector("#answer-2").addEventListener("click", function(){
+        var inputAnswer = 2;
+        validateAnswer(inputAnswer, correctAnswer);
+    })
+    document.querySelector("#answer-3").addEventListener("click", function(){
+        var inputAnswer = 3;
+        validateAnswer(inputAnswer, correctAnswer);
+    })
+    document.querySelector("#answer-4").addEventListener("click", function(){
+        var inputAnswer = 4;
+        validateAnswer(inputAnswer, correctAnswer);
+    })
+
+}
+
+// Validate Answer
+function validateAnswer(inputAnswer, correctAnswer) {
+    if (inputAnswer === correctAnswer) {
+        alert("You are correct!");
+    } else {
+        alert("You are wrong!");
+    }
+}
 
 // Initialize Page
 startScreen();
 
-// Event listeners
-document.querySelector("#start-btn").addEventListener("click", function(){
-    timer();
-    clearContent();
-});
+
+// Quiz questions 
+function quizData(){
+    var quizArr = [
+        {
+            "question": "Question here?",
+            "answer1": "Answer 1",
+            "answer2": "Answer 2",
+            "answer3": "Answer 3",
+            "answer4": "Answer 4",
+            "correctAnswer": 1
+        },
+        {
+            "question": "Question here?",
+            "answer1": "Answer 1",
+            "answer2": "Answer 2",
+            "answer3": "Answer 3",
+            "answer4": "Answer 4",
+            "correctAnswer": 1
+        }
+    ];
+
+    return quizArr;
+}
