f841fcca430e4567c1775c236e44997ed107b91d
Graham Hall <ghall89@me.com>
2021-02-18 14:19:26 -0500

    added win and lose states

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,16 @@
+// Initialize quiz data and global variables
 var quizArr = quizData();
-console.log(quizArr);
+var q;
+var timeLeft;
 
+// Intial querey selectors
 var pageContent = document.querySelector("#content");
 var timerDisplay = document.querySelector("#timer");
 
-var timeLeft;
+
 
 // Create start screen
-var startScreen = function() {
+function startScreen() {
 
     pageContent.className = "start-screen";
 
@@ -29,31 +32,38 @@ var startScreen = function() {
     pageContent.appendChild(introEl);
     pageContent.appendChild(startBtnEl);
 
-    document.querySelector("#start-btn").addEventListener("click", function(){
-        timer();
-        clearContent();
-        quizQuestion();
+    document.querySelector("#start-btn").addEventListener("click", function () {
+        startQuiz();
     });
 }
 
+// Start Quiz
+function startQuiz() {
+    timeLeft = 40;
+    q = 0;
+    timer();
+    quizQuestion();
+}
+
 // Clear content to prepare for new content
-var clearContent = function() {
+function clearContent() {
     pageContent.innerHTML = "";
 }
 
 // Timer function
-var timer = function(){
-    timeLeft = 40;
-
+function timer() {
     timerDisplay.textContent = timeLeft;
-    var countdown = setInterval(function(){
+    var countdown = setInterval(function () {
+
+        if (quizArr.length <= q) {
+            clearInterval(countdown);
+        }
+
         if (timeLeft > 0) {
             timeLeft--;
             timerDisplay.textContent = timeLeft;
         } else {
-            alert("Time's up!");
-            clearContent();
-            startScreen();
+            youLose();
             clearInterval(countdown);
         }
     }, 1000);
@@ -62,73 +72,118 @@ var timer = function(){
 }
 
 // Draw questions
-var quizQuestion = function(){
-    q = 0;
+function quizQuestion() {
+    clearContent();
 
-    var correctAnswer = quizArr[q].correctAnswer;
+    if (quizArr.length <= q) {
+        youWin();
+    } else {
+        var correctAnswer = quizArr[q].correctAnswer;
 
-    pageContent.className = "question-screen";
+        pageContent.className = "question-screen";
 
-    var quizQuestion = document.createElement("p")
-    quizQuestion.textContent = quizArr[q].question;
-    pageContent.appendChild(quizQuestion);
+        var quizQuestion = document.createElement("p")
+        quizQuestion.textContent = quizArr[q].question;
+        pageContent.appendChild(quizQuestion);
 
-    // Generate answers
-    for(i = 0; i < 4; i++) {
-        var quizAnswer = document.createElement("button");
-        if (i === 0) {
-            quizAnswer.id = "answer-1"
-            quizAnswer.textContent = quizArr[q].answer1;
-        } else if (i === 1) {
-            quizAnswer.id = "answer-2"
-            quizAnswer.textContent = quizArr[q].answer2;
-        } else if (i === 2) {
-            quizAnswer.id = "answer-3"
-            quizAnswer.textContent = quizArr[q].answer3;
-        } else if (i === 3) {
-            quizAnswer.id = "answer-4"
-            quizAnswer.textContent = quizArr[q].answer4;
+        // Generate answers
+        for (i = 0; i < 4; i++) {
+            var quizAnswer = document.createElement("button");
+            if (i === 0) {
+                quizAnswer.id = "answer-1"
+                quizAnswer.textContent = quizArr[q].answer1;
+            } else if (i === 1) {
+                quizAnswer.id = "answer-2"
+                quizAnswer.textContent = quizArr[q].answer2;
+            } else if (i === 2) {
+                quizAnswer.id = "answer-3"
+                quizAnswer.textContent = quizArr[q].answer3;
+            } else if (i === 3) {
+                quizAnswer.id = "answer-4"
+                quizAnswer.textContent = quizArr[q].answer4;
+            }
+            pageContent.appendChild(quizAnswer);
         }
-        pageContent.appendChild(quizAnswer);
+
+        // Event listeners for users answer
+        document.querySelector("#answer-1").addEventListener("click", function () {
+            var inputAnswer = 1;
+            validateAnswer(inputAnswer, correctAnswer);
+        })
+        document.querySelector("#answer-2").addEventListener("click", function () {
+            var inputAnswer = 2;
+            validateAnswer(inputAnswer, correctAnswer);
+        })
+        document.querySelector("#answer-3").addEventListener("click", function () {
+            var inputAnswer = 3;
+            validateAnswer(inputAnswer, correctAnswer);
+        })
+        document.querySelector("#answer-4").addEventListener("click", function () {
+            var inputAnswer = 4;
+            validateAnswer(inputAnswer, correctAnswer);
+        })
     }
-
-    document.querySelector("#answer-1").addEventListener("click", function(){
-        var inputAnswer = 1;
-        validateAnswer(inputAnswer, correctAnswer);
-    })
-    document.querySelector("#answer-2").addEventListener("click", function(){
-        var inputAnswer = 2;
-        validateAnswer(inputAnswer, correctAnswer);
-    })
-    document.querySelector("#answer-3").addEventListener("click", function(){
-        var inputAnswer = 3;
-        validateAnswer(inputAnswer, correctAnswer);
-    })
-    document.querySelector("#answer-4").addEventListener("click", function(){
-        var inputAnswer = 4;
-        validateAnswer(inputAnswer, correctAnswer);
-    })
-
 }
 
-// Validate Answer
+// Validate and either move on to next question or subtract from timer
 function validateAnswer(inputAnswer, correctAnswer) {
     if (inputAnswer === correctAnswer) {
         alert("You are correct!");
+        q++;
+        quizQuestion();
     } else {
         alert("You are wrong!");
+        timeLeft = timeLeft - 10;
     }
+}
+
+function youWin() {
+    clearContent();
+
+    var winTitleEl = document.createElement("h1");
+    winTitleEl.textContent = "You Win!"
+
+    var winTextEl = document.createElement("p");
+    winTextEl.textContent = "You finished the quiz with a score of " + timeLeft + "!";
+
+    var playBtnEl = document.createElement("button");
+    playBtnEl.id = "play-again";
+    playBtnEl.textContent = "Play Again!";
+
+    pageContent.appendChild(winTitleEl);
+    pageContent.appendChild(winTextEl);
+    pageContent.appendChild(playBtnEl);
+
+    document.querySelector("#play-again").addEventListener("click", function () {
+        startQuiz();
+    });
+}
+
+function youLose() {
+    clearContent();
+
+    var loseTitleEl = document.createElement("h1");
+    loseTitleEl.textContent = "You Lose!"
+
+    var playBtnEl = document.createElement("button");
+    playBtnEl.id = "play-again";
+    playBtnEl.textContent = "Play Again!";
+
+    pageContent.appendChild(loseTitleEl);
+    pageContent.appendChild(playBtnEl);
+
+    document.querySelector("#play-again").addEventListener("click", function () {
+        startQuiz();
+    });
 }
 
 // Initialize Page
 startScreen();
 
-
 // Quiz questions 
-function quizData(){
-    var quizArr = [
-        {
-            "question": "Question here?",
+function quizData() {
+    var quizArr = [{
+            "question": "This is the first question!",
             "answer1": "Answer 1",
             "answer2": "Answer 2",
             "answer3": "Answer 3",
@@ -136,12 +191,12 @@ function quizData(){
             "correctAnswer": 1
         },
         {
-            "question": "Question here?",
+            "question": "This is the second question!",
             "answer1": "Answer 1",
             "answer2": "Answer 2",
             "answer3": "Answer 3",
             "answer4": "Answer 4",
-            "correctAnswer": 1
+            "correctAnswer": 3
         }
     ];
 
